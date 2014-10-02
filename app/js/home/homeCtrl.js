5246c456e19f60e2cf7fd82bf1aa1e8221f0fb2f
kajdav <poppa.bear.joe@gmail.com>
2014-10-02 17:33:28 -0600

    creates login and signup forms.  introduces validation for login form, begins validation for signup form. signup form needs finished validation.  the click variables on the scope need to be fixed.

--- app/js/home/homeCtrl.js
+++ app/js/home/homeCtrl.js
@@ -1,6 +1,20 @@
 var app = angular.module('devSocial');
 
 app.controller('homeCtrl', function($scope) {
-
+	$scope.showLogin = false;
+	$scope.showSignup = false;
+	$scope.loginClick = false;
+	$scope.toggleShowLogin = function() {
+		$scope.showLogin = true;
+		$scope.showSignup=false;
+		$scope.signupClick = false;
+	}
+	$scope.toggleShowSignup = function() {
+		$scope.showSignup = true;
+		$scope.showLogin = false;
+		$scope.loginClick = false;
+		$scope.inputEmail = '';
+		$scope.inputPassword = '';
+	}
 
 })
